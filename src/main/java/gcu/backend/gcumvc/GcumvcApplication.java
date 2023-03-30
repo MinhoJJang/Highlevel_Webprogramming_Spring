920fb156dd98206da345378adf2911830e8f32ef
MinhoJJang <ahchjang@naver.com>
2023-03-30 15:48:28 +0900

    0330 GCU exammple

---
+++ src/main/java/gcu/backend/gcumvc/GcumvcApplication.java
@@ -0,0 +1,13 @@
+package gcu.backend.gcumvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // 해당 프레임워크를 사용하겠다는 의미
+public class GcumvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcumvcApplication.class, args);
+    }
+
+}
