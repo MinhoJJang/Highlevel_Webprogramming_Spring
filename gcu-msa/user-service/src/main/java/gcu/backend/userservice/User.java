62c4c312c3caf002f495aec21761e9410d8da695
MinhoJJang <ahchjang@naver.com>
2023-04-16 14:02:08 +0900

    0416test2

---
+++ gcu-msa/user-service/src/main/java/gcu/backend/userservice/User.java
@@ -0,0 +1,47 @@
+package gcu.backend.userservice;
+
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@SuppressWarnings("JpaDataSourceORMInspection")
+@Data
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@Entity
+public class User {
+
+    @jakarta.persistence.Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "age", nullable = false)
+    private Integer age;
+    @Lob
+    @Column(name = "contents")
+    private String contents;
+    @Builder
+    public User(String name, String address, Integer age, String contents) {
+        this.name = name;
+        this.address = address;
+        this.age = age;
+        this.contents = contents;
+    }
+
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
